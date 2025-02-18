a86594af566efd2ee7b2136f92529fd4156e2f19
Jianguo Ma <jiangma@microsoft.com>
2025-02-18 12:27:26 +0800

    remove manual operation on session map
    Signed-off-by: Jianguo Ma <jiangma@microsoft.com>

--- java-app-jcache/src/main/java/cafe/web/view/Cafe.java
+++ java-app-jcache/src/main/java/cafe/web/view/Cafe.java
@@ -1,10 +1,8 @@
 package cafe.web.view;
 
-import java.io.IOException;
 import java.io.Serializable;
 
 import jakarta.enterprise.context.SessionScoped;
-import jakarta.faces.context.FacesContext;
 import jakarta.inject.Named;
 
 @Named
@@ -16,20 +14,12 @@ public class Cafe implements Serializable {
     private String name;
 
     public String getName() {
+        System.out.println("getName() called: " + name);
         return name;
     }
 
     public void setName(String name) {
+        System.out.println("setName() called: " + name);
         this.name = name;
-    }
-
-    public String getNameFromSession() {
-        Object obj = FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("name");
-        return obj == null ? "" : obj.toString();
-    }
-
-    public void submit() throws IOException {
-        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("name", name);
-        FacesContext.getCurrentInstance().getExternalContext().redirect("");
     }
 }
